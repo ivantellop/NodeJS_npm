ec11763e87c9380bf53ec10aa0e85c7254e36b66
ivantellop <tellezlopezcesar@gmail.com>
2025-05-18 17:50:03 -0600

    Agrego mi archivo planetas.js

---
+++ data/planetas.js
@@ -0,0 +1,6 @@
+module.exports = [
+    { nombre: "Tierra", descripcion: "Nuestro hogar azul 🌍" },
+    { nombre: "Marte", descripcion: "El planeta rojo, futuro destino humano 🔴" },
+    { nombre: "Júpiter", descripcion: "El gigante gaseoso con tormentas ⚡" },
+    { nombre: "Saturno", descripcion: "Famoso por sus anillos majestuosos 💍" }
+];
